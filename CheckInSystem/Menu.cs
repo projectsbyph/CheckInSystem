0a84b1f252fa40c0e034ed5382789e774cdee80b
projectsbyph <philipj@live.se>
2026-02-09 13:05:06 +0100

    added airline options

--- CheckInSystem/Menu.cs
+++ CheckInSystem/Menu.cs
@@ -1,10 +1,56 @@
-﻿using System;
+﻿using Spectre.Console;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CheckInSystem
 {
-    internal class Menu
+    public class Menu
     {
+        public static void CheckInOptions()
+        {
+            var options = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Please select an option:")
+                    .AddChoices(new[] { "Check-In", "Exit" }));
+
+            switch (options)
+            {
+                case "Check-In":
+                    DisplayAirlines();
+                    break;
+                case "Exit":
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+
+
+        public static void DisplayAirlines()
+        {
+            AnsiConsole.MarkupLine("[bold yellow]Welcome to the Check-In System![/]");
+
+            var airlineList = new List<string> { "Scandinavian Airlines (SAS)", "Norwegian", "Sunclass Airlines", "British Airways", "Emirates", "Qatar Airways", "Finnair", "PopulAir", "Eurowings", "Lufthansa", "Brussels Airlines", "Icelandair", "Thai Airways", "Air France", "NyxAir", "Delta Airlines", "United Airlines", "Norse Atlantic", "TUI", "Air China", "All Nippon Airways (ANA)", "Iberia", "Ryanair", "TAP" }
+                .Select(f => f.ToUpper())
+                .ToList();
+
+            var airline = AnsiConsole.Prompt(
+                new SelectionPrompt<string>()
+                    .Title("Select an [green]airline[/]:")
+                    .PageSize(40)
+                    .AddChoices(airlineList));
+
+            AnsiConsole.MarkupLine("[bold blue]You selected: [green]{0}[/][/]", airline);
+
+        }
+
+        public static void DisplayFlights(string airline)
+        {
+            // This method can be implemented to display flights for the selected airline
+        }
+
+
+
+
     }
 }
--- CheckInSystem/Program.cs
+++ CheckInSystem/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Spectre.Console;
+
+namespace CheckInSystem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu.CheckInOptions();
+                AnsiConsole.MarkupLine("[bold green]Press any key to return to the menu...[/]");
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
